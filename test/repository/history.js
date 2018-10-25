43038f1656939d995759431eb65abd0c5e3c6ce6
umano <concon.wolf@gmail.com>
2018-10-25 12:51:53 +0900

    lane repo, access service

--- test/repository/history.js
+++ test/repository/history.js
@@ -5,7 +5,11 @@ const cls = new Repo();
 async function test () {
   const add = await cls.add(1,2)
   const find = await cls.findByCard(1)
+  const find2 = await cls.findByCard(1, 1, false)
+  const find3 = await cls.findByCard(9999999, 1, false)
   console.log(add)
   console.log(find)
+  console.log(find2)
+  console.log(find3)
 }
 const hoge = test()
