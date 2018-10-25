0c82f29a9cb2713669f63b02ed918667f63a25be
umano <concon.wolf@gmail.com>
2018-10-25 21:12:36 +0900

    基本的なゲート管理機能APIを実装

---
+++ routes/gate.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const PassageService = require('../lib/service/PassageService.js')
+
+const passService = new PassageService()
+
+/* GET users listing. */
+// / => 直近n個の通過履歴を取得
+// /history/:cardId => get.指定されたカードの5個ぐらいの履歴を取得
+  //*実装未定 /history/:lane => get.指定されたレーンの5個ぐらいの履歴を取得
+// /pass/master => post.総合ゲートを通過。params={cardId, lane}
+// /pass/slave/:lane => post.個別レーンを通過。params={cardId, lane}
+
+router.get('/', async function(req, res, next) {
+  try{
+    const message = await passService.getAll();
+    res.status(200);
+    res.send(message.object);
+  } catch(e){
+    console.log(e)
+    res.status(500)
+    res.send();
+  }
+});
+router.get('/history/:card', async function(req, res, next) {
+  try {
+    const message = await passService.findByCard(req.params.card);
+    res.status(200)
+    res.send(message.object)
+  } catch (e){
+    console.log(e)
+    res.status(500);
+    res.send();
+  }
+});
+router.post('/pass/master', async function(req, res, next) {
+  const card = req.body.card;
+  const lane = req.body.lane;
+  const message = await passService.accessMaster(card, lane);
+
+  if(message.isErr){
+    res.status(message.httpCode)
+  } else {
+    res.status(200)
+  }
+  res.send(message.object)
+})
+router.post('/pass/slave/:lane', async function(req, res, next) {
+  //validationしよう！　な！
+  const card = req.body.card;
+  const lane = req.params.lane;
+  const message = await passService.accessSlave(card, lane);
+  if(message.isErr){
+    res.status(message.httpCode)
+  } else {
+    res.status(200)
+  }
+  // ここで監視画面に通知がほしい => 多分websocket
+  res.send()
+})
+
+module.exports = router;
+
