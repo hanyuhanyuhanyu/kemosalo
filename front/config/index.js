48514bcf1548d843fe8c3f07487f31227ee62db0
umano <concon.wolf@gmail.com>
2018-10-26 19:35:48 +0900

    frontのindex.jsのproxy設定を追記

--- front/config/index.js
+++ front/config/index.js
@@ -14,9 +14,10 @@ module.exports = {
       '/api': {
         target: 'http://localhost:3000/',
         changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
-        }
+      },
+      '/socket.io': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
       }
     },
 
