f0102b3ceae1621d41ce8824ede99ca3ba46d958
umano <concon.wolf@gmail.com>
2018-11-06 08:29:17 +0900

    logの追加/脱落時にアニメーション

--- front/src/components/AccessLog.vue
+++ front/src/components/AccessLog.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="accessLog" class='accessLog'>
-    <div class='each-log log-box' v-for='log in logs' :key='log.id'>
-      <div class="data-box">
-        <div class="columns">
-          <div class="column">Card</div>
-          <div class="column">Time</div>
+    <transition-group name='logs'>
+        <div class='each-log log-box logs-item' v-for='log in logs' :key='log.id'>
+          <div class="data-box">
+            <div class="columns">
+              <div class="column">Card</div>
+              <div class="column">Time</div>
+            </div>
+            <div class="rows">
+              <div class="row">{{log.card}}</div>
+              <div class="row">{{log.time.split(' ')[1]}}</div>
+            </div>
+          </div>
+          <div class="data-box">
+            <div class="columns">
+              <div class="column">Gate</div>
+              <div class="column">IP</div>
+            </div>
+            <div class="rows">
+              <div class="row">{{log.name}}</div>
+              <div class="row">{{log.ip}}</div>
+            </div>
+          </div>
         </div>
-        <div class="rows">
-          <div class="row">{{log.card}}</div>
-          <div class="row">{{log.time.split(' ')[1]}}</div>
-        </div>
-      </div>
-      <div class="data-box">
-        <div class="columns">
-          <div class="column">Gate</div>
-          <div class="column">IP</div>
-        </div>
-        <div class="rows">
-          <div class="row">{{log.name}}</div>
-          <div class="row">{{log.ip}}</div>
-        </div>
-      </div>
-    </div>
+    </transition-group>
   </div>
 </template>
 
@@ -39,11 +41,21 @@ export default {
 </script>
 
 <style>
+/* .logs-move{ */
+/*   transition: transform 0.3s; */
+/* } */
 #accessLog{
   width: 30%;
 }
 .each-log{
   font-size: 1.5rem;
+  transition: all 0.3s;
+}
+.logs-enter{
+  transform: translateX(-100%);
+}
+.logs-leave-to{
+  opacity: 0;
 }
 .data-box{
   display: flex;
