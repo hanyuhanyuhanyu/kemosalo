4e616108e4c66c5f8b40961b2b247fae1914b380
umano <concon.wolf@gmail.com>
2018-11-13 09:04:35 +0900

    通過履歴APIがユーザ名も返すようになった

--- routes/card.js
+++ routes/card.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const CardService = require('../lib/service/CardService.js')
+const UserService = require('../lib/service/userService.js')
 const WebSocket = require('../lib/websocket/websocket.js');
 
 const cardService = new CardService();
+const userService = new UserService();
 const webSocket = new WebSocket();
 
 router.get('/read/:card', async function(req, res, next) {
   try{
+    const card = await userService.findByCard(req.params.card);
+    if(!card){
+      res.status(404);
+      res.send('このカードは未登録です')
+      return
+    }
     const histories = await cardService.getAllHistories(req.params.card);
-    //本来ならカードの存在チェックをすべきだがマスターテーブルがないため不能
     res.status(200);
     webSocket.broadcastCardHistory({
       card: req.params.card,
